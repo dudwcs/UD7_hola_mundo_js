241464c5fca8083dcdd34909509ac0f78cd60de7
dudwcs <dudwcs@gmail.com>
2024-02-28 14:01:43 +0100

    await async

--- holaMundo.php
+++ holaMundo.php
@@ -37,7 +37,7 @@
             <p>Este es el footer</p>
         </footer>
         
-        <script src="js/script1.js" type="text/javascript"></script>
-        
+        <!-- <script src="js/script1.js" type="text/javascript"></script> -->
+         <script src="js/fetch/fetch_get.js" type="text/javascript"></script>
     </body>
 </html>
--- js/fetch/fetch_get.js
+++ js/fetch/fetch_get.js
@@ -1,7 +1,11 @@
 window.onload = function () {
     onceLoaded();
-
 };
+
+
+// window.onload = async function () {   
+//     await onceLoadedAwait();
+// };
 
 //https://reqres.in/ API de prueba
 function onceLoaded() {
@@ -9,21 +13,49 @@ function onceLoaded() {
         .then(data => data.json())
         .then(users => {
             data = users.data;
-            console.log(data);
-
-
-            let lista = document.createElement("ul");
-            for (var i = 0; i < data.length; i++) {
-                let elem = document.createElement("li");
-                elem.innerHTML = data[i].email;
-                lista.appendChild(elem);
-            }
-
-            var body = document.getElementById("users");
-            body.innerHTML = "";
-            body.appendChild(lista);
+            procesarJSON(data);
         }
         )
         .catch((err) => console.error("error:", err));
 
+}
+
+
+async function onceLoadedAwait() {
+
+    //https://reqres.in/ API de prueba
+
+    try {
+        let response = await fetch('https://reqres.in/api/users');
+        if (response.ok === true) {
+            let users = await response.json();
+            data = users.data;
+            procesarJSON(data);
+
+        }
+
+        else {
+            console.error("Error-HTTP: " + response.status);
+        }
+    }
+    catch (error) {
+        console.error("Error: " + error);
+    }
+}
+
+function procesarJSON(data) {
+   
+    console.log(data);
+
+
+    let lista = document.createElement("ul");
+    for (var i = 0; i < data.length; i++) {
+        let elem = document.createElement("li");
+        elem.innerHTML = data[i].email;
+        lista.appendChild(elem);
+    }
+
+    var body = document.getElementById("users");
+    body.innerHTML = "";
+    body.appendChild(lista);
 }
